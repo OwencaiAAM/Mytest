c9999c8a156a0c78f9b726f89f4c42d3bf4c1f72
OWEN CAI <owencai2000@gmail.com>
2018-01-02 17:04:14 +0800

    Test 1

---
+++ TestForm/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TestForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInitial = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnInventory = new System.Windows.Forms.Button();
+            this.btnAuthenticate = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInitial
+            // 
+            this.btnInitial.Location = new System.Drawing.Point(23, 25);
+            this.btnInitial.Name = "btnInitial";
+            this.btnInitial.Size = new System.Drawing.Size(96, 32);
+            this.btnInitial.TabIndex = 0;
+            this.btnInitial.Text = "初始化";
+            this.btnInitial.UseVisualStyleBackColor = true;
+            this.btnInitial.Click += new System.EventHandler(this.btnInitial_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(23, 77);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(96, 32);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "打开读卡器";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnInventory
+            // 
+            this.btnInventory.Location = new System.Drawing.Point(23, 132);
+            this.btnInventory.Name = "btnInventory";
+            this.btnInventory.Size = new System.Drawing.Size(96, 32);
+            this.btnInventory.TabIndex = 2;
+            this.btnInventory.Text = "盘点标签";
+            this.btnInventory.UseVisualStyleBackColor = true;
+            this.btnInventory.Click += new System.EventHandler(this.btnInventory_Click);
+            // 
+            // btnAuthenticate
+            // 
+            this.btnAuthenticate.Location = new System.Drawing.Point(23, 261);
+            this.btnAuthenticate.Name = "btnAuthenticate";
+            this.btnAuthenticate.Size = new System.Drawing.Size(96, 32);
+            this.btnAuthenticate.TabIndex = 4;
+            this.btnAuthenticate.Text = "卡认证";
+            this.btnAuthenticate.UseVisualStyleBackColor = true;
+            this.btnAuthenticate.Click += new System.EventHandler(this.btnAuthenticate_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(23, 197);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(96, 32);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "卡连接";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(135, 197);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(96, 32);
+            this.btnDisconnect.TabIndex = 6;
+            this.btnDisconnect.Text = "断开连接";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(135, 77);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(96, 32);
+            this.btnClose.TabIndex = 7;
+            this.btnClose.Text = "关闭读卡器";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(23, 328);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(96, 32);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "读卡";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 395);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnAuthenticate);
+            this.Controls.Add(this.btnInventory);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnInitial);
+            this.Name = "Form1";
+            this.Text = "MF102Utest";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInitial;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnInventory;
+        private System.Windows.Forms.Button btnAuthenticate;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnRead;
+    }
+}
+
---
+++ TestForm/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PluginCommon;
+
+namespace TestForm
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList pluginsList = new ArrayList();
+        private IPlugin _plugins = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInitial_Click(object sender, EventArgs e)
+        {
+            _plugins.RunFuction("InitCardReader");
+
+            //MF102UMain.InitCardReader();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+           
+            string type = "USB";
+            string port = "COM1";
+            string baud = "9600";
+            bool re = Convert.ToBoolean(_plugins.RunFuctionRes("OpenCardReader", new string[] { type, port, baud }));
+            if (re)
+            {
+                MessageBox.Show("读卡器打开成功");
+            }
+            else
+            {
+                MessageBox.Show("读卡器打开失败");
+            }
+
+            //MF102UMain.OpenCardReader("USB");
+        }
+
+        private void btnInventory_Click(object sender, EventArgs e)
+        {
+            string uid = _plugins.RunFuctionRes("Inventory").ToString();
+            MessageBox.Show(uid);
+        }
+
+        private void btnAuthenticate_Click(object sender, EventArgs e)
+        {
+            string block = "11";
+            string type = "0";
+            string keyA = "FFFFFFFFFFFF";
+            string keyB = "FFFFFFFFFFFF";
+
+            bool ire = Convert.ToBoolean(_plugins.RunFuctionRes("Authenticate", new string[] { block, type, keyA, keyB }));
+
+            if (ire)
+            {
+                MessageBox.Show("认证成功！");
+            }
+            else
+            {
+                MessageBox.Show("认证失败！");
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            bool ire = Convert.ToBoolean(_plugins.RunFuctionRes("ConnectCard"));
+            if (ire)
+            {
+                MessageBox.Show("连接成功！");
+            }
+            else
+            {
+                MessageBox.Show("连接失败！");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            _plugins.RunFuction("Close");
+            //MF102UMain.Close();
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            _plugins.RunFuction("DisConnect");
+            //MF102UMain.DisConnect();
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            string block = "11";
+            string type = "0";
+            string keyA = "FFFFFFFFFFFF";
+            string keyB = "FFFFFFFFFFFF";
+            string cardno = _plugins.RunFuctionRes("ReadCard", new string[] { block, type, keyA, keyB }).ToString();
+            MessageBox.Show(cardno);
+            //MessageBox.Show(MF102UMain.ReadCard(0,0));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadAllPlugs();
+            _plugins = this.pluginsList[0] as IPlugin;
+
+        }
+
+        ///<summary>
+        /// 载入所有插件
+        ///</summary>
+        private void LoadAllPlugs()
+        {
+            //获取插件目录(plugins)下所有文件
+            string path = System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Plugins";
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                if (file.ToUpper().EndsWith(".DLL"))
+                {
+                    try
+                    {
+                        //载入dll
+                        Assembly ab = Assembly.LoadFrom(file);
+                        Type[] types = ab.GetTypes();
+                        foreach (Type t in types)
+                        {
+                            //如果某些类实现了预定义的IMsg.IMsgPlug接口，则认为该类适配与主程序(是主程序的插件)
+                            if (t.GetInterface("IPlugin") != null)
+                            {
+                                pluginsList.Add(ab.CreateInstance(t.FullName));
+                                //listBox1.Items.Add(t.FullName);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+    }
+}
